d42a4abc7077282a3cb849c33b7276395c3be9ce
GeraldOKoth <okothgerald449@gmail.com>
2025-08-29 18:11:53 +0300

    fix: Replace parameters leading to code breaks

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,11 +1,14 @@
-const WeatherCard = ({ title, value, icon }) => {
+const WeatherCard = ({ day, temp, icon }) => {
   return (
-    <div className="bg-black text-white rounded-lg p-4 flex flex-col items-center justify-center w-32">
+    <div className="bg-blue-50 text-black rounded-lg mb-4 p-4 flex items-center justify-between w-12/12">
+      <div >
+        <p className="text-sm font-bold">{day}</p>
+        <span>Aug</span>
+      </div>
+      <p className="text-lg font-bold">{temp}</p>
       <span className="material-icons text-blue-400 mb-2">{icon}</span>
-      <p className="text-sm">{title}</p>
-      <p className="text-lg font-bold">{value}</p>
     </div>
   );
-}
+};
 
-export default WeatherCard
+export default WeatherCard;
