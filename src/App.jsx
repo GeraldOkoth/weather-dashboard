799a3c4a573f8d4133adcf91e46680f5489f2b28
GeraldOKoth <okothgerald449@gmail.com>
2025-08-29 17:30:34 +0300

    Configure routing for various components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import Dashboard from "./pages/Dashboard";
+import ErrorMessage from './pages/ErrorMessage';
+import ResetPassword from './pages/ResetPassword';
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/error" element={<ErrorMessage />} />
+        <Route path="/forgot" element={<ResetPassword />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
