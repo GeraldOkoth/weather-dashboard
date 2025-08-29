679051d07dc97c60b49d13af281e4267a3a0c911
GeraldOKoth <okothgerald449@gmail.com>
2025-08-29 18:00:28 +0300

    fix: Add react icons and login button

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,21 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import InputField from "../components/InputField";
+import {FaEnvelope, FaLock, FaPersonBooth, FaArrowLeft, FaGoogle, FaFacebook} from 'react-icons/fa'
+
 
 const SignUpPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200 p-6">
-      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
-        <button className="mb-4 text-gray-500">←</button>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
+      <div className="w-full max-w-md bg-gray-300 shadow-md rounded-lg p-6">
+        <button className="mb-4 text-gray-500 border rounded-2xl p-1.5">
+          {<FaArrowLeft />}
+        </button>
 
         <form className="space-y-4">
-          <InputField icon="person" type="text" placeholder="Enter Your Name" />
           <InputField
-            icon="email"
+            icon={<FaPersonBooth />}
+            type="text"
+            placeholder="Enter Your Name"
+          />
+          <InputField
+            icon={<FaEnvelope />}
             type="email"
             placeholder="Enter Your Email"
           />
           <InputField
-            icon="lock"
+            icon={<FaLock />}
             type="password"
             placeholder="Enter Your Password"
           />
@@ -30,26 +38,30 @@ const SignUpPage = () => {
             </Link>
           </div>
 
+          <button className="w-full py-2 rounded-full bg-blue-500 border-0 text-white font-bold">
+            <NavLink to="/dashboard">Sign Up</NavLink>
+          </button>
+
           <div className="flex items-center justify-center text-gray-400">
             <span className="flex-1 border-b"></span>
             <span className="px-2">or</span>
             <span className="flex-1 border-b"></span>
           </div>
 
-          <button className="w-full py-2 rounded-full bg-white border flex items-center justify-center gap-2">
-            <img src="/google-icon.png" alt="Google" className="w-5 h-5" />
+          <button className="w-full py-2 rounded-full bg-white border-0 flex items-center justify-center gap-2">
+            {<FaGoogle />}
             Google
           </button>
 
-          <button className="w-full py-2 rounded-full bg-white border flex items-center justify-center gap-2">
-            <img src="/facebook-icon.png" alt="Facebook" className="w-5 h-5" />
+          <button className="w-full py-2 rounded-full bg-white border-0 flex items-center justify-center gap-2">
+            {<FaFacebook />}
             Facebook
           </button>
 
           <p className="text-center text-sm mt-4">
-            Don’t have an account?{" "}
-            <Link to="/signup" className="text-blue-500">
-              sign up
+            Have an account?{" "}
+            <Link to="/" className="text-blue-500">
+              Login
             </Link>
           </p>
         </form>
