b3203f6e0e2f7a422f1c7082de86e0385110ee7f
GeraldOKoth <okothgerald449@gmail.com>
2025-08-29 17:54:31 +0300

    fix: Add react icons and login button

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,20 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import InputField from "../components/InputField";
+import { FaEnvelope, FaLock, FaFacebook, FaGoogle } from "react-icons/fa";
 
 const LoginPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200 p-6">
-      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
+      <div className="w-full max-w-md bg-gray-300 shadow-2xl rounded-lg p-6">
         <h2 className="text-center text-xl font-semibold mb-4">Welcome Back</h2>
 
         <form className="space-y-4">
           <InputField
-            icon="email"
+            icon={<FaEnvelope />}
             type="email"
             placeholder="Enter Your Email"
           />
           <InputField
-            icon="lock"
+            icon={<FaLock />}
             type="password"
             placeholder="Enter Your Password"
           />
@@ -29,32 +30,36 @@ const LoginPage = () => {
             </Link>
           </div>
 
+          <button className="w-full py-2 rounded-full bg-blue-500 border-0 text-white font-bold">
+            <NavLink to="/dashboard">Login</NavLink>
+          </button>
+
           <div className="flex items-center justify-center text-gray-400">
             <span className="flex-1 border-b"></span>
             <span className="px-2">or</span>
             <span className="flex-1 border-b"></span>
           </div>
 
-          <button className="w-full py-2 rounded-full bg-white border flex items-center justify-center gap-2">
-            <img src="/google-icon.png" alt="Google" className="w-5 h-5" />
+          <button className="w-full py-2 rounded-full bg-white border-0 flex items-center justify-center gap-2">
+            {<FaGoogle />}
             Google
           </button>
 
-          <button className="w-full py-2 rounded-full bg-white border flex items-center justify-center gap-2">
-            <img src="/facebook-icon.png" alt="Facebook" className="w-5 h-5" />
+          <button className="w-full py-2 rounded-full bg-white border-0 flex items-center justify-center gap-2">
+            {<FaFacebook />}
             Facebook
           </button>
 
           <p className="text-center text-sm mt-4">
-            Have an account?{" "}
-            <Link to="/login" className="text-blue-500">
-              login
+            Don't have an account?{" "}
+            <Link to="/signup" className="text-blue-500">
+              Sign Up
             </Link>
           </p>
         </form>
       </div>
     </div>
   );
-}
+};
 
-export default LoginPage
+export default LoginPage;
