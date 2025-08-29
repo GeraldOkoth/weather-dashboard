280a79bbfc6e709d36ccc1870eccda9335019a99
GeraldOKoth <okothgerald449@gmail.com>
2025-08-29 16:59:07 +0300

    Add ResetPassword page

---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import InputField from "../components/InputField";
+import { NavLink } from "react-router-dom";
+import { FaEnvelope, FaLock, FaCheck } from "react-icons/fa";
+
+
+const ResetPassword = () => {
+  return (
+    <div className="xl:w-4/12 flex flex-col items-center justify-center mt-4 mx-16 bg-gray-300 p-6 rounded-2xl shadow-2xl">
+      <h2 className="text-center text-xl font-semibold mb-4">Reset Password</h2>
+      <form className="space-y-4">
+        <InputField
+          icon={<FaEnvelope />}
+          type="email"
+          placeholder="Enter Your Email"
+        />
+        <InputField
+          icon={<FaLock />}
+          type="password"
+          placeholder="Enter Your Password"
+        />
+        <InputField
+          icon={<FaLock />} 
+          type="password"
+          placeholder="Repeat Your Password"
+        />
+
+        <button className="w-full py-2 rounded-full bg-blue-500 border-0 text-white font-bold">
+          <NavLink to="/">Reset Password</NavLink>
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ResetPassword;
